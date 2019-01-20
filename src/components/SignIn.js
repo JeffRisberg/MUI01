9304f29c748b910f92fbcd5bcbfec8b100126a80
JeffRisberg <jrisberg@aol.com>
2019-01-19 22:55:57 -0800

    began to understand how styles work.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,89 +1,67 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-//import LockOutlinedIcon from 'material-ui-icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
-const styles = theme => ({
-    main: {
-        width: 'auto',
-        display: 'block', // Fix IE 11 issue.
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-            width: 400,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
+const styles = {
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
     },
-    paper: {
-        marginTop: theme.spacing.unit * 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+};
+
+const currencies = [
+    {
+        value: 'USD',
+        label: '$',
     },
-    avatar: {
-        margin: theme.spacing.unit,
-        backgroundColor: theme.palette.secondary.main,
+    {
+        value: 'EUR',
+        label: '€',
     },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing.unit,
+    {
+        value: 'BTC',
+        label: '฿',
     },
-    submit: {
-        marginTop: theme.spacing.unit * 3,
+    {
+        value: 'JPY',
+        label: '¥',
     },
-});
+];
 
 function SignIn(props) {
     const { classes } = props;
+    const [values, setValues] = React.useState({
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+    });
 
     return (
-        <main className={classes.main}>
-            <CssBaseline />
-            <Paper className={classes.paper}>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <form className={classes.form}>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="email">Email Address</InputLabel>
-                        <Input id="email" name="email" autoComplete="email" autoFocus />
-                    </FormControl>
-                    <FormControl margin="normal" required fullWidth>
-                        <InputLabel htmlFor="password">Password</InputLabel>
-                        <Input name="password" type="password" id="password" autoComplete="current-password" />
-                    </FormControl>
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign in
-                    </Button>
-                </form>
-            </Paper>
-        </main>
+        <form className={classes.container} noValidate autoComplete="off">
+            <TextField
+                id="standard-name"
+                label="Name"
+                className={classes.textField}
+                value={values.name}
+                margin="normal"
+            />
+            <TextField
+                id="standard-uncontrolled"
+                label="Uncontrolled"
+                defaultValue="foo"
+                className={classes.textField}
+                margin="normal"
+            />
+        </form>
     );
 }
 
-SignIn.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default SignIn;
+export default withStyles(styles)(SignIn);
