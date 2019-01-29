c59162e55082ef4622b9ec691e1080a1a9d3097f
JeffRisberg <jrisberg@aol.com>
2019-01-28 22:15:08 -0800

    added another grid example

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Footer from './Footer';
 import UserList from './UserList';
 import SignIn from './SignIn';
 import GridSet from './GridSet';
+import GridSet2 from './GridSet2';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
@@ -51,6 +52,7 @@ const App = props => (
         </Grid>
         <UserList/>
         <GridSet/>
+        <GridSet2/>
         <SignIn/>
         <Footer/>
     </div>
---
+++ src/components/GridSet2.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    }
+});
+
+class GridSet2 extends React.Component {
+    render() {
+        const {classes} = this.props;
+        const spacing = 16;
+
+        return (
+            <Grid container spacing={24}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>xs=12</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                </Grid>
+            </Grid>
+            < /div>
+        )
+    }
+}
+
+GridSet2.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(GridSet2);
