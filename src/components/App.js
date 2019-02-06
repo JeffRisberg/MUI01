4106634eb63faf0a8ed7dcca5b34b7f20a4da762
JeffRisberg <jrisberg@aol.com>
2019-02-05 22:53:51 -0800

    began to integrate router

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import UserList from './UserList';
 import SignIn from './SignIn';
 import CourseList from './CourseList';
 import { withStyles } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const styles = {
     root: {
@@ -22,12 +25,19 @@ const styles = {
 };
 
 const App = props => (
-    <div>
-        <UserList/>
-        <CourseList />
-        <SignIn/>
-        <Footer/>
-    </div>
+    <MuiThemeProvider theme={createMuiTheme()}>
+        <Router>
+            <Route path='/' component={Navigation} />
+        </Router>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={CourseList} />
+                <Route path="/registration" component={Signin} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/users" component={UserList} />
+            </Switch>
+        </Router>
+    </MuiThemeProvider>
 );
 
 export default withStyles(styles)(App);
