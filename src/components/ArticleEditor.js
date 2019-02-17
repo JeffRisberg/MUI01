0bbd0b981c9649ef62ec64e2cfcd66a10ca138da
JeffRisberg <jrisberg@aol.com>
2019-02-17 11:00:17 -0800

    open the dialog

--- src/components/ArticleEditor.js
+++ src/components/ArticleEditor.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
-
-import {FormGroup, TextField} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import {Editor} from '@tinymce/tinymce-react';
 
 import * as OverlayActions from '../actions/Overlay';
 import {bindActionCreators} from 'redux';
@@ -49,6 +48,7 @@ class ArticleEditor extends Component {
 
         return (
             <Dialog
+                fullWidth={true}
                 open={open}
                 keepMounted
                 onClose={this.handleCancel}
@@ -59,17 +59,13 @@ class ArticleEditor extends Component {
                     Enter article text
                 </DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="edit-dialog-description">
-                        Use Rich text editing
-                    </DialogContentText>
-                    <form>
-                        <FormGroup>
-                            <TextField
-                                label='Your Text'
-                                value={state.text || ''}
-                            />
-                        </FormGroup>
-                    </form>
+                    <Editor name="text"
+                            initialValue="<p>Enter your article here</p>"
+                            init={{
+                                plugins: 'link image code',
+                                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                            }}
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this.handleSave} color="primary" disabled={loading}>
