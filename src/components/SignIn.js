dd333439bbffe69234ba3d7053075ce3c4fabcb0
JeffRisberg <jrisberg@aol.com>
2019-02-18 18:45:36 -0800

    minor update

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/styles';
 import TextField from '@material-ui/core/TextField';
+import {withStyles} from '@material-ui/styles';
 
 const styles = {
     root: {
