bb0eeee93497d9528dedf5a1d069e795280ce16c
JeffRisberg <jrisberg@aol.com>
2019-03-12 16:55:03 -0700

    fixed up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = {
-    root: {
-        marginTop: "10px",
-        padding: "5px",
-        background: "#eee"
-    },
+   root: {
+      marginTop: '10px',
+      padding: '6px',
+      background: '#eee'
+   },
 };
 
 function Footer(props) {
-    const {classes} = props;
-    const spacing = 16;
+   const {classes} = props;
 
-    return (
-        <div className={classes.root}>
-            MUI01
-        </div>
-    );
+   return (
+      <div className={classes.root}>
+         MUI01
+      </div>
+   );
 }
+
+Footer.propTypes = {
+   classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Footer);
