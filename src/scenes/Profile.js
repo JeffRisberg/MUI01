e24164d00573257d09ebe488c65db12ef27ee188
JeffRisberg <jrisberg@aol.com>
2019-03-12 14:53:01 -0700

    updated profile screen

--- src/scenes/Profile.js
+++ src/scenes/Profile.js
@@ -1,37 +1,81 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography'
-import SignIn from '../components/SignIn'
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 
 /**
- * the Profile screen, with sign-in
+ * the Profile screen, showing user information and user tags
  */
 const styles = (theme) => ({
-    grid: {
-        height: '50px',
-    }
+   grid: {
+      height: '50px',
+   }
 });
 
 class Profile extends Component {
 
-    render() {
-        return (
-            <div>
-                <Card>
-                    <CardContent>
-                        <Typography gutterBottom variant="headline" component="h2">
-                            Welcome
-                        </Typography>
-                        <Typography component="p">
-                            Article manager
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </div>
-        )
-    }
+   render() {
+      const {classes} = props;
+
+      return (
+         <div>
+            <Card>
+               <CardContent>
+                  <Typography gutterBottom variant="headline" component="h2">
+                     Your Profile
+                  </Typography>
+                  <form className={classes.container} noValidate autoComplete="off">
+                     <TextField
+                        id="standard-name"
+                        label="Name"
+                        className={classes.textField}
+                        value="Jack Bauer"
+                        margin="normal"
+                     />
+                     <TextField
+                        id="standard-uncontrolled"
+                        label="Address Line 1"
+                        defaultValue="123 Main Street"
+                        className={classes.textField}
+                        margin="normal"
+                     />
+                     <TextField
+                        id="standard-uncontrolled"
+                        label="Address Line 2"
+                        defaultValue=""
+                        className={classes.textField}
+                        margin="normal"
+                     />
+                     <TextField
+                        id="standard-uncontrolled"
+                        label="City or Town"
+                        defaultValue="Palo Alto"
+                        className={classes.textField}
+                        margin="normal"
+                     />
+                     <TextField
+                        id="standard-uncontrolled"
+                        label="State"
+                        defaultValue="CA"
+                        className={classes.textField}
+                        margin="normal"
+                     />
+                  </form>
+                  <Typography component="p">
+                     Your Tags
+                  </Typography>
+               </CardContent>
+            </Card>
+         </div>
+      )
+   }
 }
+
+Profile.propTypes = {
+   classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Profile);
--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -3,36 +3,36 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import SignIn from '../components/SignIn'
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 
 /**
  * the welcome screen, with sign-in
  */
 const styles = (theme) => ({
-    grid: {
-        height: '50px',
-    }
+   grid: {
+      height: '50px',
+   }
 });
 
 class Splash extends Component {
 
-    render() {
-        return (
-            <div>
-                <Card>
-                    <CardContent>
-                        <Typography gutterBottom variant="headline" component="h2">
-                            Welcome
-                        </Typography>
-                        <Typography component="p">
-                            Article manager
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <SignIn/>
-            </div>
-        )
-    }
+   render() {
+      return (
+         <div>
+            <Card>
+               <CardContent>
+                  <Typography gutterBottom variant="headline" component="h2">
+                     Welcome
+                  </Typography>
+                  <Typography component="p">
+                     Article manager
+                  </Typography>
+               </CardContent>
+            </Card>
+            <SignIn/>
+         </div>
+      )
+   }
 }
 
 export default withStyles(styles)(Splash);
